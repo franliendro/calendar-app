cfe7c513d7111297eeaf0673339b30bd25f90624
Carlos Francisco Liendro <73044721+franliendro@users.noreply.github.com>
2025-08-06 10:50:07 -0300

    project-creation

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import CalendarApp from "./Components/CalendarApp" //al parecer no hace falta la extension, seguro es porque ambas son .jsx
+import './Components/CalendarApp.css' //css
+
+
+const App = () => {
+  return (
+    <div className="container">
+      <CalendarApp></CalendarApp>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/CalendarApp.jsx
@@ -0,0 +1,18 @@
+const CalendarApp = () => {
+  return (
+    <div className="calendar-app">
+      <div className="calendar">
+        <h1 className="heading">Calendar</h1>
+        <div className="navigate-date">
+          <h2 className="month">May,</h2>
+          <h2 className="year">2025</h2>
+          <div className="buttons">
+          <div className="buttons"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default CalendarApp
